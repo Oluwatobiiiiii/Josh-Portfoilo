e90b18eb079bb6d9c356a4caae3270e067cbe32f
Treasure <mac@MACs-MacBook-Pro.local>
2023-04-05 01:24:51 +0100

    mobile view done

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,6 @@
+
+// MODAL SECTIONAS
+
 // // Get the modal
 // var modal = document.getElementById('myModal');
 
@@ -23,3 +26,41 @@
     
 //  }
 
+
+
+// IMAGE SLIDER SECTION
+
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
+const imageContainer = document.querySelector('.image-container');
+const myWork = document.querySelectorAll('.image-container img');
+let activeButton;
+
+let counter = 0;
+const size = window.innerWidth < 640? 300 : 540;
+
+
+imageContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+rightButton.addEventListener('click', () => {
+    console.log(size);
+    if (counter >= myWork.length - 1) return;
+    imageContainer.style.transition = "transform 0.5s ease-in-out";
+    counter++;
+    imageContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    leftButton.disabled = false;
+    if (counter === myWork.length - 1) {
+        rightButton.disabled = true;
+    }
+  });
+  
+  leftButton.addEventListener('click', () => {
+    if (counter <= 0) return;
+    imageContainer.style.transition = "transform 0.5s ease-in-out";
+    counter--;
+    imageContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    rightButton.disabled = false;
+    if (counter === 0) {
+      leftButton.disabled = true;
+    }
+  });
